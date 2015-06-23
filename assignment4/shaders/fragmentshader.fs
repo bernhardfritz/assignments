9f17c394f71b63e0d4ee7128d33ccf0aaf87b5ba
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2015-06-23 10:54:38 +0200

    vertex and fragment shader fix

--- assignment4/shaders/fragmentshader.fs
+++ assignment4/shaders/fragmentshader.fs
@@ -4,27 +4,33 @@ uniform int LightControl;
 uniform int TextureIndex;
 uniform vec4 LightColor;
 uniform sampler2D myTextureSampler;
+uniform vec4 LightPosition;
 
 in vec4 vColor;
 in vec3 vNormal;
 in vec3 vLight;
 in vec3 vCamera;
 in vec2 UVcoords;
+in vec3 world_coord;
+in vec3 eye_coord;
 
 out vec4 FragColor;
 void main()
 {
     vec4 tempColor = texture(myTextureSampler, UVcoords);
-    float I_D = max(0,dot(vNormal,vLight));
+
+    vec3 N = vNormal;
+    vec3 L = normalize(LightPosition.xyz - world_coord);
+    float LdotN  = dot(N,L);
+    vec3 R = reflect(-L,N);
+    float I_D = max(LdotN, 0.0);
     vec4 diffuse = I_D*vColor;
+    float I_S = max(pow(dot(normalize(-eye_coord),R),30),0.0);
+    vec4 specular = I_S*LightColor;
+
     if(TextureIndex > -1) {
       diffuse = I_D*tempColor;
     }
-
-    vec3 E = normalize(-vCamera);
-    vec3 H = normalize(vLight + E);
-    float I_S = max(dot(vNormal,H),0.0);
-    vec4 specular = pow(I_S,30)*LightColor;
 
     vec4 ambient = vec4(0.1,0.1,0.1,1.0);
 
--- assignment4/shaders/vertexshader.vs
+++ assignment4/shaders/vertexshader.vs
@@ -3,7 +3,6 @@
 uniform mat4 ProjectionMatrix;
 uniform mat4 ViewMatrix;
 uniform mat4 ModelMatrix;
-uniform vec4 LightPosition;
 
 layout (location = 0) in vec3 Position;
 layout (location = 1) in vec3 Color;
@@ -12,17 +11,25 @@ layout (location = 3) in vec2 UV;
 
 out vec4 vColor;
 out vec3 vNormal;
-out vec3 vLight;
 out vec3 vCamera;
 out vec2 UVcoords;
+out vec3 world_coord;
+out vec3 eye_coord;
 
 void main()
 {
-   gl_Position = ProjectionMatrix*ViewMatrix*ModelMatrix*vec4(Position, 1.0);
+   vec4 world_pos = ModelMatrix * vec4(Position, 1.0);
+   vec4 eye_pos = ViewMatrix * world_pos;
+   vec4 clip_pos = ProjectionMatrix * eye_pos;
+
+   world_coord = world_pos.xyz;
+   eye_coord = eye_pos.xyz;
+
    vColor = vec4(Color, 1.0);
    mat3 NormalMatrix = mat3(transpose(inverse(ModelMatrix)));
    vNormal = normalize(NormalMatrix * Normal);
-   vLight = normalize(LightPosition).xyz;
    vCamera = vec3(ViewMatrix*ModelMatrix * vec4(Position, 1.0));
    UVcoords = UV;
+
+   gl_Position = clip_pos;
 }
